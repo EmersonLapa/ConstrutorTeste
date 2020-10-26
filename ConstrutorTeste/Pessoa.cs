8559ae4b9fd9ef7a8a032fe8a05ae34dd34d005e
Emerson <emersonlapa@outlook.com>
2020-10-25 22:36:17 -0300

    Acrecentado ao git

---
+++ ConstrutorTeste/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstrutorTeste
+{
+    class Pessoa
+    {
+        public static String Nome = "EMERSON";
+        public static int Idade = 23;
+
+        public Pessoa(string nome , int idade ){
+            Nome = nome;
+            Idade = idade;
+        }
+        
+        /* public override string ToString()
+         {
+             return "Nome " + Nome + " Idade " + Idade; 
+
+         }*/
+    }
+}
---
+++ ConstrutorTeste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConstrutorTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pessoa p = new Pessoa("Emerson", 23);
+
+            Console.WriteLine("Nome " + Pessoa.Nome + " Idade " + Pessoa.Idade);
+        }
+    }
+}
